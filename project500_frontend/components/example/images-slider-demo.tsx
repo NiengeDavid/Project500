f4a948a2a59610e59aeef10122d98a5862fe7cbb
David Nienge <davidnienge@gmail.com>
2025-05-30 20:12:05 +0100

    Update images-slider-demo.tsx

--- project500_frontend/components/example/images-slider-demo.tsx
+++ project500_frontend/components/example/images-slider-demo.tsx
@@ -31,7 +31,7 @@ export default function ImagesSliderDemo() {
         <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
           Project 500!<br /> Creating dyslexia awareness in Africa
         </motion.p>
-        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdVuKa84ng8lo2ei7qz0sQgc6MzVwmeRBVUMqHR9JUoV-8N6A/viewform?usp=sharing" target="_blank" rel="noopener noreferrer" className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+        <a href="https://forms.gle/iAVHZxrkpJWTkzHQA" target="_blank" rel="noopener noreferrer" className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
           <span>Join now →</span>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </a>
